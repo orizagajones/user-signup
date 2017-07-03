498334bfa8deceab50f87a6edcdaf92a08800f66
Rhiannon Orizaga <orizaga.jones@gmail.com>
2017-07-03 15:47:21 -0700

    changes

--- main.py
+++ main.py
@@ -11,21 +11,25 @@ def index():
     return render_template('welcome.html')
 
 
-@app.route("/signup")
+@app.route("/signup", methods=['POST'])
 def signup():
-    if (request.form['username']) < 3 and (request.form['username'])>20:
+    if {{'username'}} < 3 or {{'username'}} >20:
         error1 = "Please enter a valid username."
         return redirect("/?error=" + error1)
-    if (request.form['password']) < 3 and (request.form['password'])>20:
+
+    if {{'password'}} < 3 or {{'password'}}>20:
         error2 = "Please enter a valid username."
         return redirect("/?error=" + error2)
-    if (request.form['verify_password']) != (request.form['password']):
+
+    if {{'verify_password'}} != {{'password'}}:
         error3 = "Password does not match."
         return redirect("/?error=" + error3)
-    if (request.form['email']) < 3 and (request.form['email'])>20:
+
+    if (request.form['email']) < 3 or (request.form['email'])>20:
         error4 = "Please enter a valid email address."
         return redirect("/?error=" + error4)
-    if "@" not in (request.form['email']) or if "." not in (request.form['email']):
+
+    if "@" not in {{'email'}} or if "." not in {{'email'}}:
         error4 = "Please enter a valid email address."
         return redirect("/?error=" + error4)
     return render_template('signup.html')
