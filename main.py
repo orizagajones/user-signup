b0e84e91fc11aced059a027c071d252e22708cbd
Rhiannon Orizaga <orizaga.jones@gmail.com>
2017-07-03 19:56:23 -0700

    refactor main.py

--- main.py
+++ main.py
@@ -7,29 +7,50 @@ app.config['DEBUG'] = True
 
 @app.route("/signup", methods=['POST','GET'])
 def signup():
+    if check_username == False:
+        error_message = "Please enter a valid username."
+        return (render_template('signup.html'), error_message)
+    elif check_password == False:
+        error_message = "Please enter a valid password."
+        return (render_template('signup.html'), error_message)
+    elif dblcheck_password == False:
+        error_message = "Password does not match."
+        return (render_template('signup.html'), error_message)
+    elif check_email == False:
+        error_message = "Please enter a valid email address."
+        return (render_template('signup.html'), error_message)
+    else: 
+        return (render_template('welcome.html'))
+
+
+def check_username():
     username = request.form['username']
+    if len(username) < 3 or len(username) >20 or ' ' in username:
+        return False
+    else: 
+        return True
+
+def check_password():
     password = request.form['password']
-    verify_password = request.form['verify_password']
-    email = request.form['email']
-
-    if username < 3 or username >20 or " " in username:
-        error = "Please enter a valid username."
-        return render_template('signup.html', error1 = error)
-
-    if password < 3 or password>20 or " " in password:
-        error = "Please enter a valid username."
-        return render_template('signup.html', error2 = error)
-
-    if verify_password != password:
-        error = "Password does not match."
-        return render_template('signup.html', error3 = error)
-
-    if email < 3 or email>20 or " " in email or "@" not in email or "." not in email:
-        error = "Please enter a valid email address."
-        return render_template('signup.html', error4 = error)
-
+    if len(password) < 3 or len(password) > 20 or " " in password:
+        return False
     else:
-        return render_template('welcome.html')
+        return True
+
+def dblcheck_password():
+    verify_password = request.form['verify_password']
+    if verify_password != password:
+        return False
+    else:
+        return True
+
+def check_email():
+    email = request.form['email']
+    if email != "" and len(email) < 3 or len(email) > 20 or " " in email or "@" not in email or "." not in email:
+        return False
+    else:
+        return True
+
 
 @app.route("/", methods=['post', 'get'])
 def index():
