9deb00abed3c1553e0d7b77210140eeea6e95e8b
Rhiannon Orizaga <orizaga.jones@gmail.com>
2017-07-03 19:33:18 -0700

    several changes to tidy up code

--- main.py
+++ main.py
@@ -4,36 +4,36 @@ from flask import Flask, request, redirect, render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-username = request.form['username']
-password = request.form['password']
-verify_password = request.form['verify_password']
-email = request.form['email']
 
-
-@app.route("/")
-def index():
-    return render_template('signup.html')
-
-
-@app.route("/signup", methods=['POST'])
+@app.route("/signup", methods=['POST','GET'])
 def signup():
+    username = request.form['username']
+    password = request.form['password']
+    verify_password = request.form['verify_password']
+    email = request.form['email']
+
     if username < 3 or username >20 or " " in username:
-        error1 = "Please enter a valid username."
-        return render_template('signup.html', error1 = error1)
+        error = "Please enter a valid username."
+        return render_template('signup.html', error1 = error)
 
     if password < 3 or password>20 or " " in password:
-        error2 = "Please enter a valid username."
-        return render_template('signup.html', error2 = error2)
+        error = "Please enter a valid username."
+        return render_template('signup.html', error2 = error)
 
     if verify_password != password:
-        error3 = "Password does not match."
-        return render_template('signup.html', error3= error3)
+        error = "Password does not match."
+        return render_template('signup.html', error3 = error)
 
     if email < 3 or email>20 or " " in email or "@" not in email or "." not in email:
-        error4 = "Please enter a valid email address."
-        return render_template('signup.html', error4 = error4)
+        error = "Please enter a valid email address."
+        return render_template('signup.html', error4 = error)
 
     else:
         return render_template('welcome.html')
+
+@app.route("/", methods=['post', 'get'])
+def index():
+    encoded_error = request.args.get("error")
+    return render_template('signup.html'), encoded_error and cgi.escape(encoded_error, quote=True)
 
 app.run()
