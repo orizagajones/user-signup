c07f4bc30bf33c973a03d40097a08defe2a7ca8d
Rhiannon Orizaga <orizaga.jones@gmail.com>
2017-07-03 18:00:30 -0700

    change variable names

--- main.py
+++ main.py
@@ -17,25 +17,20 @@ def index():
 
 @app.route("/signup", methods=['POST'])
 def signup():
-    if username < 3 or username >20:
+    if username < 3 or username >20 or " " in username:
         error1 = "Please enter a valid username."
-        return render_template('signup.html'), error1
+        return render_template('signup.html', error1 = error1)
 
-    if password < 3 or password>20:
+    if password < 3 or password>20 or " " in password:
         error2 = "Please enter a valid username."
-        return render_template('signup.html'), error2
+        return render_template('signup.html', error2 = error2)
 
     if verify_password != password:
         error3 = "Password does not match."
-        return render_template('signup.html'), error3
+        return render_template('signup.html', error3= error3)
 
-    if email < 3 or email>20:
+    if email < 3 or email>20 or " " in email or "@" not in email or "." not in email:
         error4 = "Please enter a valid email address."
-        return render_template('signup.html'), error4
-
-    if "@" not in email or "." not in email:
-        error4 = "Please enter a valid email address."
-        return render_template('signup.html'), error4
-
+        return render_template('signup.html', error4 = error4)
 
 app.run()
